0ef9bbb05f30f8dde52d475e1310b4394da2045e
vivi-aulia <auliavivi074@gmail.com>
2025-03-20 10:16:59 +0700

    Week 6: Canny Edge + Instance Segmentation for Lane Detection

---
+++ lane_detection.py
@@ -0,0 +1,52 @@
+from ultralytics import YOLO
+
+import cv2
+
+
+
+# Load model YOLOv8 Instance Segmentation
+
+model = YOLO("yolov8n-seg.pt")
+
+
+
+def detect_rail_lane(image_path):
+
+    """Mendeteksi jalur rel menggunakan YOLOv8 Instance Segmentation"""
+
+    results = model(image_path, show=True)
+
+    results[0].save("lane_detection_result.jpg")
+
+
+
+# Contoh penggunaan
+
+detect_rail_lane("rel kereta api.jpg")
+
+from ultralytics import YOLO
+
+import cv2
+
+
+
+# Load model YOLOv8 Instance Segmentation
+
+model = YOLO("yolov8n-seg.pt")
+
+
+
+def detect_rail_lane(image_path):
+
+    """Mendeteksi jalur rel menggunakan YOLOv8 Instance Segmentation"""
+
+    results = model(image_path, show=True)
+
+    results[0].save("lane_detection_result.jpg")
+
+
+
+# Contoh penggunaan
+
+detect_rail_lane("rel kereta api.jpg")
+
